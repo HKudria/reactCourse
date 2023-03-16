4c72b3428c3712d9a352362c32a3e28351da807a
Herman Kudria <h.kudrya@hotmail.com>
2023-03-16 18:58:12 +0100

    feat: create 404 error page

--- reactComponent1/src/main.tsx
+++ reactComponent1/src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {NotFoundPage} from './components/NotFoundPage/NotFoundPage';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+        <React.StrictMode>
+            <Routes>
+                <Route path="/" element={<App/>}/>
+                <Route path="/aboutUs" />
+                <Route path="/aboutUs" />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+
+        </React.StrictMode>
+    </BrowserRouter>,
 )
