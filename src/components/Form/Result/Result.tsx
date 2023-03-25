b940f8f233813d9c12d574e7d3f5b106b968a47c
Herman Kudria <h.kudrya@hotmail.com>
2023-03-25 12:35:22 +0100

    feat: fix checkbox view

--- src/components/Form/Result/Result.tsx
+++ src/components/Form/Result/Result.tsx
@@ -9,7 +9,7 @@ export default class Result extends React.Component<FormResult> {
                 <li className="list-group-item">Text: {this.props.text}</li>
                 <li className="list-group-item">Date: {this.props.date}</li>
                 <li className="list-group-item">Select: {this.props.select}</li>
-                <li className="list-group-item">Checkbox: {this.props.checkbox}</li>
+                <li className="list-group-item">Checkbox: {this.props.checkbox ? 'true' : 'false'}</li>
                 <li className="list-group-item">File: {this.props.file}</li>
                 <li className="list-group-item">Switcher: {this.props.switcher}</li>
             </ul>
