c30409b8d59f9c32c39b77a8ee8a48f46bd949d9
Herman Kudria <h.kudrya@hotmail.com>
2023-03-31 21:36:10 +0200

    feat: rewriteForm with hooks

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,189 +1,86 @@
 import s from './Form.module.css'
-import React, {ChangeEvent} from 'react';
+import React, {useState} from 'react';
 import Result from './Result/Result';
+import {SubmitHandler, useForm} from 'react-hook-form';
 
-interface FormStateInterface {
+export interface FormInterface {
     text: string,
     date: string,
     select: string,
     checkbox: boolean,
     switcher: string,
-    isSubmit: boolean,
-    file: React.RefObject<HTMLInputElement>
-    error: FormError
-    sumbtions: FormResult[]
+    file: FileList
 }
 
-export interface FormResult {
-    text: string,
-    date: string,
-    select: string,
-    checkbox: boolean,
-    switcher: string,
-    file: string
+
+const Form = () => {
+    const {register, handleSubmit, reset, formState: {errors}} = useForm();
+
+    const onSubmit: SubmitHandler<FormInterface> = data => {
+        setResults([...results, data]);
+        reset()
+    }
+
+    const [results, setResults] = useState<FormInterface[]>([]);
+
+    return (
+        <div className={s.wrapper}>
+            <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-group">
+                    <label htmlFor="text" className="form-label">Enter text</label>
+                    <input type="text" className="form-control" id="text" placeholder="Enter text"
+                           {...register("text", {required: true})}/>
+                    {errors.text && <div className="invalid-feedback d-block">Fill the text</div>}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="date" className="form-label">Select date</label>
+                    <input type="date" className="form-control" id="date" {...register("date", {required: true})}/>
+                    {errors.date && <div className="invalid-feedback d-block">Date should be field</div>}
+                </div>
+                <div className="form-check">
+                    <label className="form-label" htmlFor="checkbox">Check me out</label>
+                    <input type="checkbox" className="form-check-input"
+                           id="checkbox" {...register("checkbox", {required: true})}/>
+                    {errors.checkbox && <div className="invalid-feedback d-block">Check checkbox should be</div>}
+                </div>
+                <div className="form-group">
+                    <label className="form-label" htmlFor="select">Chose select option</label><br/>
+                    <select className="form-select form-select-sm" {...register("select", {required: true})}>
+                        <option></option>
+                        <option value="grapefruit">Grapefruit</option>
+                        <option value="lime">Lime</option>
+                        <option value="coconut">Coconut</option>
+                        <option value="mango">Mango</option>
+                    </select>
+                    {errors.select && <div className="invalid-feedback d-block">Select should be selected</div>}
+                </div>
+                <div className="form-group">
+                    <label className="form-label" htmlFor="file">Upload file</label><br/>
+                    <input type="file" className="form-control" id="file" {...register("file", {required: true})}/>
+                    {errors.file && <div className="invalid-feedback d-block">Fill should be uploaded</div>}
+                </div>
+                <div className="form-check">
+                    <input className="form-check-input" type="radio" value="radio1"
+                           id="flexRadioDefault1" {...register("switcher", {required: true})}/>
+                    <label className="form-check-label" htmlFor="flexRadioDefault1">
+                        Default radio 1
+                    </label>
+                </div>
+                <div className="form-check">
+                    <input className="form-check-input" type="radio" value="radio2"
+                           id="flexRadioDefault2" {...register("switcher", {required: true})}/>
+                    <label className="form-check-label" htmlFor="flexRadioDefault2">
+                        Default radio 2
+                    </label>
+                </div>
+                {errors.switcher && <div className="invalid-feedback d-block">Switcher should be chosen</div>}
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+            {results.map((el, index) => <Result key={index} text={el.text} select={el.select}
+                                                date={el.date} file={el.file} checkbox={el.checkbox}
+                                                switcher={el.switcher}/>)}
+        </div>
+    );
 }
 
-interface FormError {
-    text?: string,
-    date?: string,
-    select?: string,
-    checkbox?: string,
-    switcher?: string,
-    file?: string
-}
-
-export default class Form extends React.Component {
-    state: FormStateInterface = {
-        text: '',
-        date: '',
-        select: '',
-        checkbox: false,
-        isSubmit: false,
-        switcher: 'radio1',
-        file: React.createRef(),
-        error: {},
-        sumbtions: []
-    };
-
-    onChangeText = (event: ChangeEvent<HTMLInputElement>) => {
-        this.setState({...this.state, text: event.target.value})
-    };
-
-    onChangeDate = (event: ChangeEvent<HTMLInputElement>) => {
-        this.setState({...this.state, date: event.target.value})
-    };
-
-    onChangeSelect = (event: ChangeEvent<HTMLSelectElement>) => {
-        this.setState({...this.state, select: event.target.value})
-    };
-
-    onChangeCheckbox = () => {
-        this.setState({...this.state, checkbox: !this.state.checkbox})
-    };
-
-    onChangeSwitcher = (event: ChangeEvent<HTMLInputElement>) => {
-        this.setState({...this.state, switcher: event.target.value})
-    };
-
-    onSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        const validation = this.validateForm()
-        if (validation.valid) {
-            this.setState({...this.state, isSubmit: true, error: validation.error})
-            this.addSubmittedValue()
-        } else {
-            this.setState({...this.state, isSubmit: false, error: validation.error})
-        }
-
-    }
-
-    validateForm = () => {
-        const error: FormError = {}
-        if (this.state.text.length === 0) error.text = 'Fill text';
-        if (this.state.date.length === 0) error.date = 'Fill date';
-        if (!this.state.checkbox) error.checkbox = 'Check checkbox';
-        if (!this.state.select) error.select = 'Select should be selected';
-        if (!this.state.switcher) error.switcher = 'Switcher should be chosen';
-        if (this.state.file.current && this.state.file.current.files && this.state.file.current.files.length === 0) error.file = 'Fill should be uploaded';
-        if (Object.keys(error).length === 0) {
-            return {valid: true, error: {}}
-        } else {
-            return {valid: false, error: error}
-        }
-    }
-
-    addSubmittedValue = () => {
-        if (Object.keys(this.state.error).length === 0) {
-            const date = new Date(this.state.date)
-            const result: FormResult  = {
-                date: date.toLocaleString(),
-                text: this.state.text,
-                select: this.state.select,
-                checkbox: this.state.checkbox,
-                file: (this.state.file.current && this.state.file.current.files && this.state.file.current.files[0].name) ?? '',
-                switcher: this.state.switcher
-            }
-
-            this.setState({
-                    ...this.state,
-                    sumbtions: [...this.state.sumbtions, result],
-                    text: '',
-                    date: '',
-                    checkbox: false,
-                    switcher: 'radio1',
-                    isSubmit: false,
-                    error: {}
-                },
-            );
-            if (this.state.file.current) this.state.file.current.value = '';
-        }
-        return (<></>)
-    }
-
-    render() {
-        return (
-            <div className={s.wrapper}>
-                <form className={s.form} onSubmit={this.onSubmitForm}>
-                    <div className="form-group">
-                        <label htmlFor="text" className="form-label">Enter text</label>
-                        <input type="text" className="form-control" id="text" placeholder="Enter text"
-                               onChange={this.onChangeText} value={this.state.text}/>
-                        {this.state.error.text &&
-                            <div className="invalid-feedback d-block">{this.state.error.text}</div>}
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="date" className="form-label">Select date</label>
-                        <input type="date" className="form-control" id="date" onChange={this.onChangeDate}
-                               value={this.state.date}/>
-                        {this.state.error.date &&
-                            <div className="invalid-feedback d-block">{this.state.error.date}</div>}
-                    </div>
-                    <div className="form-check">
-                        <label className="form-label" htmlFor="checkbox">Check me out</label>
-                        <input type="checkbox" className="form-check-input" id="checkbox"
-                               onChange={this.onChangeCheckbox} checked={this.state.checkbox}/>
-                    </div>
-                    {this.state.error.checkbox &&
-                        <div className="invalid-feedback d-block">{this.state.error.checkbox}</div>}
-                    <div className="form-group">
-                        <label className="form-label" htmlFor="select">Chose select option</label><br/>
-                        <select value={this.state.select} id="select" onChange={this.onChangeSelect}
-                                className="form-select form-select-sm">
-                            <option></option>
-                            <option value="grapefruit">Grapefruit</option>
-                            <option value="lime">Lime</option>
-                            <option value="coconut">Coconut</option>
-                            <option value="mango">Mango</option>
-                        </select>
-                        {this.state.error.select &&
-                            <div className="invalid-feedback d-block">{this.state.error.select}</div>}
-                    </div>
-                    <div className="form-group">
-                        <label className="form-label" htmlFor="file">Upload file</label><br/>
-                        <input type="file" className="form-control" id="file" ref={this.state.file}/>
-                        {this.state.error.file &&
-                            <div className="invalid-feedback d-block">{this.state.error.file}</div>}
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="radio" value="radio1" id="flexRadioDefault1"
-                               onChange={this.onChangeSwitcher} checked={this.state.switcher === 'radio1'}/>
-                        <label className="form-check-label" htmlFor="flexRadioDefault1">
-                            Default radio 1
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="radio" value="radio2" id="flexRadioDefault2"
-                               onChange={this.onChangeSwitcher} checked={this.state.switcher === 'radio2'}/>
-                        <label className="form-check-label" htmlFor="flexRadioDefault2">
-                            Default radio 2
-                        </label>
-                    </div>
-                    {this.state.error.switcher &&
-                        <div className="invalid-feedback d-block">{this.state.error.switcher}</div>}
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
-                {this.state.sumbtions.map((el, index) => <Result key={index} text={el.text} select={el.select} date={el.date} file={el.file} checkbox={el.checkbox} switcher={el.switcher}/>)}
-            </div>
-        );
-    }
-}
+export default Form;
--- src/components/Form/Result/Result.tsx
+++ src/components/Form/Result/Result.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import {FormResult} from '../Form';
+import {FormInterface} from '../Form';
 
-export default class Result extends React.Component<FormResult> {
-
-    render() {
-        return (
-            <ul className={'list-group'}>
-                <li className="list-group-item">Text: {this.props.text}</li>
-                <li className="list-group-item">Date: {this.props.date}</li>
-                <li className="list-group-item">Select: {this.props.select}</li>
-                <li className="list-group-item">Checkbox: {this.props.checkbox ? 'true' : 'false'}</li>
-                <li className="list-group-item">File: {this.props.file}</li>
-                <li className="list-group-item">Switcher: {this.props.switcher}</li>
-            </ul>
-        );
-    }
+const Result = (props: FormInterface) => {
+    return (
+        <ul className={'list-group'}>
+            <li className="list-group-item">Text: {props.text}</li>
+            <li className="list-group-item">Date: {props.date}</li>
+            <li className="list-group-item">Select: {props.select}</li>
+            <li className="list-group-item">Checkbox: {props.checkbox ? 'true' : 'false'}</li>
+            <li className="list-group-item">File: {props.file && props.file[0].name}</li>
+            <li className="list-group-item">Switcher: {props.switcher}</li>
+        </ul>
+    );
 }
+
+export default Result;
