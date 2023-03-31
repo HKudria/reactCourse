0c9df753d83975b7aeebb3c2b1ac87f1ebbf2487
Herman Kudria <h.kudrya@hotmail.com>
2023-03-31 21:59:46 +0200

    feat: update component to use hook

--- src/components/Header/Search/Search.tsx
+++ src/components/Header/Search/Search.tsx
@@ -1,29 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-interface SearchState {
-    searchValue: string
-}
+const Search = () => {
+    const [searchValue, setSearchValue] = useState('');
 
-export default class Search extends React.Component {
-    state: SearchState = {
-        searchValue: ''
-    };
+    useEffect(() => {
+        setSearchValue(localStorage.getItem('searchValue') ?? '')
+    }, []);
 
-    componentDidMount() {
-        this.setState({searchValue: localStorage.getItem('searchValue') ?? ''});
-    }
 
-    onChangeSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({searchValue: event.target.value});
+    const onChangeSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(event.target.value);
         localStorage.setItem('searchValue', event.target.value)
     }
 
-    render() {
-        return (
-            <div className="form-inline my-2 my-lg-0 me-5">
-                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"
-                       value={this.state.searchValue} onChange={this.onChangeSearch}/>
-            </div>
-        )
-    }
+    return (
+        <div className="form-inline my-2 my-lg-0 me-5">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"
+                   value={searchValue} onChange={onChangeSearch}/>
+        </div>
+    )
 }
+
+export default Search;
--- src/components/Main/Card/Card.tsx
+++ src/components/Main/Card/Card.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import s from './Card.module.css'
 import {CardData} from '../Main';
 
-export default class Card extends React.Component<CardData> {
+const Card = (props: CardData) => {
 
-    render() {
-        return (
-            <div className={`${s.fontSize} card`}>
-                <img className="card-img-top" src={this.props.imgPath} alt={this.props.name}/>
-                <div className="card-body">
-                    <h5 className="card-title">{this.props.name}</h5>
-                    <p className="card-text">{this.props.content}</p>
-                </div>
+    return (
+        <div className={`${s.fontSize} card`}>
+            <img className="card-img-top" src={props.imgPath} alt={props.name}/>
+            <div className="card-body">
+                <h5 className="card-title">{props.name}</h5>
+                <p className="card-text">{props.content}</p>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+export default Card;
